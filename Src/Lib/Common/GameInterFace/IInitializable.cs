1732c74ebdce887d99f686830655cee3ef260088
wjf <wjf3543203728@163.com>
2026-05-01 18:26:28 +0800

    补充

---
+++ Src/Lib/Common/GameInterFace/IInitializable.cs
@@ -0,0 +1,10 @@
+﻿namespace GameInterFace
+{
+    /// <summary>
+    /// LoadingManager 初始化时会尝试调用所有继承该接口的Single类的 Init 方法。
+    /// </summary>
+    public interface IInitializable
+    {
+        public void Init();
+    }
+}
